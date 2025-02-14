c4eee137d9e88ab04be3216d26fef03077316749
Alok8138 <126945574+Alok8138@users.noreply.github.com>
2025-02-14 09:47:04 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Signup() {
+  return (
+    <div>
+      signup-finel
+    </div>
+  )
+}
+
+// git remote add origin https://github.com/Alok8138/cmapus-Commute.git
